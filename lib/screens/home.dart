e4332c801a9ad4b63a66c35c694962945ed476f9
Deivissondb <deivissondb@hotmail.com>
2023-01-10 11:33:45 -0300

    save

---
+++ lib/screens/home.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:alubank/components/sections/account_actions.dart';
+import 'package:alubank/components/sections/account_points.dart';
+import 'package:alubank/components/sections/header.dart';
+import 'package:alubank/components/sections/recent_activity.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        children: const <Widget>[
+          Header(),
+          RecentActivity(),
+          AccountActions(),
+          AccountPoints(),
+        ],
+      ),
+    ));
+  }
+}
